75c8772b8bbfa32e7515aa2b799591e182766e8c
waldohidalgo <whidalgohp@gmail.com>
2024-05-21 16:01:46 -0400

    config to deploy render

--- api/index.js
+++ api/index.js
@@ -30,7 +30,7 @@ app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
   });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, function () {
   console.log("Your app is listening on port " + port);
 });
